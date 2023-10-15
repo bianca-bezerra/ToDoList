df343543de3711f7ad98588c1128d456466d1e0c
biancabezerrathe71@gmail.com <biancabezerrathe71@gmail.com>
2023-10-14 23:14:45 -0300

    Deploy dockerfile

---
+++ src/main/java/br/com/biancabezerra/todolist/user/UserController.java
@@ -0,0 +1,36 @@
+package br.com.biancabezerra.todolist.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
+    
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel UserModel){
+        var user = this.userRepository.findByUsername(UserModel.getUsername());
+
+        if(user != null){
+           
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+        var passwordHashred = BCrypt.withDefaults().hashToString(12,UserModel.getPassword().toCharArray());
+
+        UserModel.setPassword(passwordHashred);
+        
+        var userCreated = this.userRepository.save(UserModel);
+        return ResponseEntity.status(HttpStatus.OK).body(userCreated);
+    }
+}
